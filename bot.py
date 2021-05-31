5692178021248ea360da2c9b0456c00bffdd6bfe
Anton-Euro <62991246+Anton-Euro@users.noreply.github.com>
2021-05-31 18:17:35 +0300

    new

--- bot.py
+++ bot.py
@@ -5,13 +5,13 @@ from aiogram import Bot, Dispatcher, executor, types
 from selenium import webdriver
 import os
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
+options = webdriver.ChromeOptions()
+options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+options.add_argument('--headless')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--no-sandbox')
 
-br = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+br = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=options)
 
 logging.basicConfig(level=logging.INFO)
 
