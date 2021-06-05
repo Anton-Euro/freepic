d284ef4e0c01e8969e304e483129f34be29e492e
Anton-Euro <62991246+Anton-Euro@users.noreply.github.com>
2021-06-05 13:26:30 +0300

    new

--- bot.py
+++ bot.py
@@ -3,15 +3,11 @@ import logging
 import asyncio
 from aiogram import Bot, Dispatcher, executor, types
 from selenium import webdriver
-import os
 
 options = webdriver.ChromeOptions()
-options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
 options.add_argument('--headless')
-options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--no-sandbox')
 
-br = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=options)
+br = webdriver.Chrome(options=options)
 
 logging.basicConfig(level=logging.INFO)
 
@@ -25,7 +21,7 @@ async def start_pars(message: types.Message):
 		br.get('https://www.epicgames.com/store/ru/free-games')
 		html = br.find_element_by_class_name('css-1442lgn-CardGrid-styles__group').text
 		await message.answer(html)
-		await asyncio.sleep(86400)
-
+		await asyncio.sleep(10)
+#86400
 if __name__ == '__main__':
 	executor.start_polling(dp, skip_updates=True)
